722426dabfcc635c056de8cb7a81219f5121c478
Alexia Huntsman <125164538+AlexiaHuntsman@users.noreply.github.com>
2026-03-24 12:10:59 -0600

    Update analyzer.py
    added new feature. will alert to brute force attempts and display username

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,4 @@
+failed_login_count = {}
 # Open the log file and read all lines
 with open("sample_logs.txt", "r") as file:
     lines = file.readlines()
@@ -9,8 +10,21 @@ print()
 for line in lines:
     clean_line = line.strip()
 
-    # Check if line contains a failed login
+    # Check if the line contains a failed login
     if "LOGIN_FAILURE" in clean_line:
         print("ALERT: Failed login detected!")
         print(clean_line)
-        print()
+
+        #extract username
+        if "user=" in clean_line:
+            parts = clean_line.split("user=")
+            if len(parts) > 1:
+                username = parts[1].split()[0]
+
+        if username not in failed_login_count:
+            failed_login_count[username] = 0
+
+        failed_login_count[username] += 1
+
+        if failed_login_count[username] >= 3:
+            print(f"BRUTE FORCE ALERT: {username}) has {failed_login_count[username]} failed login attempts!")
